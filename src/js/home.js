6a0b4a2481ca7ed530edafaf2309f6e34f7f48df
KHAD4444 <alexandresomingues@gmail.com>
2025-12-03 04:17:49 -0300

    Primeiro commit do meu projeto

---
+++ src/js/home.js
@@ -0,0 +1,15 @@
+function myd() {
+  window.location.href = "src/pages/myd.html";
+}
+
+function exp() {
+  window.location.href = "src/pages/exp.html";
+}
+
+function obj() {
+  window.location.href = "src/pages/obj.html";
+}
+
+function mtv() {
+  window.location.href = "src/pages/mtv.html";
+}
